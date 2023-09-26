527e1a0733bc56cf1f2087b8acea412a09109ca6
sjin7777 <so-jin@naver.com>
2023-09-27 00:21:44 +0900

    commit

--- src/useState/05.useTabs.js
+++ src/useState/05.useTabs.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const content = [
     {
@@ -10,11 +11,27 @@ const content = [
     }
 ];
 
+const useTabs = (initialTab, allTabs) => {
+    const[currentIndex, setCurrentIndex] = useState(initialTab);
+    
+    if(!allTabs || !Array.isArray(allTabs)) { 
+        return; 
+    }
+    
+    return {
+        currentItem: allTabs[currentIndex],
+        changeItem: setCurrentIndex
+    }
+};
 
 function App() {
+    const {currentItem, changeItem} = useTabs(0, content);
     return (
         <div>
-            {content.map((section, index) => <button key={index}>{section.tab}</button>)}
+            {content.map((section, index) => (
+                <button key={index} onClick={() => changeItem(index)}>{section.tab}</button>
+            ))}
+            <div>{currentItem.content}</div>
         </div>
     );
 }
